58f37f2ccd6aa80a2e878a3dcf29e9aed713013b
Ryan Champ <ryan.champ@gmail.com>
2016-08-04 12:56:07 -0500

    Added fragment that will hold the poster grid and wired temp data to MainActivity

--- app/src/main/java/com/icecoldedge/popmovies/MainActivity.java
+++ app/src/main/java/com/icecoldedge/popmovies/MainActivity.java
@@ -5,9 +5,17 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PosterGridFragment())
+                    .commit();
+        }
     }
 }
